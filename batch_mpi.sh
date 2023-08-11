6fae3789b44070e8b3f4bc06e8cda15084d57461
Sam Sutcliffe <sam.sutcliffe@.dur.ac.uk>
2023-08-11 09:54:52 +0100

    Updated mpi build step

--- batch_mpi.sh
+++ batch_mpi.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # Request resources:
-#SBATCH -c 32     # 1 entire node
+#SBATCH -c 1     # 1 entire node
 #SBATCH --time=12:00:0  # 6 hours (hours:minutes:seconds)
 #SBATCH --mem=64G      # 1 GB RAM
+#SBATCH --gres=tmp:1G
 #SBATCH -p shared
+#SBATCH -n 2
+#SBATCH -N 2
 
 module load gcc
+#module load openmpi
+module load intelmpi
 module load aocl
-module load openmpi
 
 echo "Running code"
 rm output/*
 
-sbcl --dynamic-space-size 64000  --disable-debugger --load "build_step.lisp" --quit
+sbcl --dynamic-space-size 4000  --disable-debugger --load "build_step.lisp" --quit
+
+cp ~/quicklisp/local-projects/cl-mpm-worker/mpi-worker ./
+
+mpirun ./mpi-worker
+#mpirun ~/quicklisp/local-projects/cl-mpm-worker/mpi-worker
 
--- build_step.lisp
+++ build_step.lisp
@@ -1,8 +1,23 @@
-(ql:quickload :cl-mpm-worker)
+;(ql:quickload :cl-mpm-worker)
 (ql:quickload "cl-mpm/examples/slump")
+(require 'cl-mpm-worker)
 
 (defun cl-mpm-worker::primary-main ()
   (format t "Running MPI with ~D jobs~%"  (cl-mpi::mpi-comm-size))
   (cl-mpm/examples/slump::mpi-run (cl-mpi::mpi-comm-size)))
 
-(cl-mpm-worker::build)
+
+;(build)
+(asdf:make :cl-mpm-worker)
+;lfarm-server 
+;(ql:quickload :lfarm-server)
+;(defun main (&rest args)
+;  (print "hello")
+;   (lfarm-server:start-server "127.0.0.1" 11111)
+;  )
+;(sb-ext:save-lisp-and-die
+;   "mpi-worker"
+;    :executable t
+;    :toplevel #'main
+;    :save-runtime-options t)
+(uiop:quit)
