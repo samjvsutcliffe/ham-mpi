076a983f3db056d40f4248838219cec29ea84ab6
Sam Sutcliffe <sam.sutcliffe@.dur.ac.uk>
2023-08-11 19:47:39 +0100

    Update test file

--- batch_mpi.sh
+++ batch_mpi.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Request resources:
-#SBATCH -c 1     # 1 entire node
+#SBATCH -c 4     # 1 entire node
 #SBATCH --time=12:00:0  # 6 hours (hours:minutes:seconds)
-#SBATCH --mem=64G      # 1 GB RAM
-#SBATCH --gres=tmp:1G
+#SBATCH --mem=4G      # 1 GB RAM
 #SBATCH -p shared
-#SBATCH -n 2
-#SBATCH -N 2
+#SBATCH -n 1
+#SBATCH -N 1
 
 module load gcc
 #module load openmpi
@@ -21,6 +20,6 @@ sbcl --dynamic-space-size 4000  --disable-debugger --load "build_step.lisp" --qu
 
 cp ~/quicklisp/local-projects/cl-mpm-worker/mpi-worker ./
 
-mpirun -n 4 ./mpi-worker
+mpirun ./mpi-worker
 #mpirun ~/quicklisp/local-projects/cl-mpm-worker/mpi-worker
 
